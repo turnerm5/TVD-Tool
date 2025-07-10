741d996ab4b86fd12ea976495d11c684a7fc8244
Marshall Turner <turnerm5@gmail.com>
2025-07-09 22:04:24 -0700

    Updated sampleData.js to revise project budgets, square footages, and component costs across phases and benchmarks, reflecting the latest project specifications and adjustments.

--- data/sampleData.js
+++ data/sampleData.js
@@ -1,88 +1,91 @@
 const sampleData = {
     "phases": {
         "phase1": {
-            "totalProjectBudget": 20000000,
-            "projectAreaSF": 30000,
+            "totalProjectBudget": 18000000,
+            "projectAreaSF": 22500,
             "components": [
-                { "name": "A Substructure", "square_footage": 30000, "current_rom": 183.33 },
-                { "name": "D Services", "square_footage": 30000, "current_rom": 333.34 },
-                { "name": "F Special Construction", "square_footage": 10000, "current_rom": 200.00 },
-                { "name": "G Building Sitework", "square_footage": 30000, "current_rom": 166.66 }
+                { "name": "A Substructure", "square_footage": 22500, "current_rom": 140.00 },
+                { "name": "B Shell", "square_footage": 22500, "current_rom": 0 },
+                { "name": "C Interiors", "square_footage": 22500, "current_rom": 0 },
+                { "name": "D Services", "square_footage": 22500, "current_rom": 0 },
+                { "name": "E Equipment and Furnishings", "square_footage": 22500, "current_rom": 0 },
+                { "name": "F Special Construction", "square_footage": 40000, "current_rom": 102 },
+                { "name": "G Building Sitework", "square_footage": 40000, "current_rom": 65.00 }
             ]
         },
         "phase2": {
-            "totalProjectBudget": 40000000,
-            "projectAreaSF": 60000,
+            "totalProjectBudget": 38000000,
+            "projectAreaSF": 49050,
             "components": [
-                { "name": "A Substructure", "square_footage": 60000, "current_rom": 160 },
-                { "name": "B Shell", "square_footage": 60000, "current_rom": 170 },
-                { "name": "C Interiors", "square_footage": 60000, "current_rom": 135 },
-                { "name": "D Services", "square_footage": 60000, "current_rom": 200 },
-                { "name": "E Equipment and Furnishings", "square_footage": 60000, "current_rom": 45 },
-                { "name": "F Special Construction", "square_footage": 10000, "current_rom": 100 },
-                { "name": "G Building Sitework", "square_footage": 30000, "current_rom": 80 }
+                { "name": "A Substructure", "square_footage": 49050, "current_rom": 120.00 },
+                { "name": "B Shell", "square_footage": 49050, "current_rom": 150.00 },
+                { "name": "C Interiors", "square_footage": 49050, "current_rom": 140.00 },
+                { "name": "D Services", "square_footage": 49050, "current_rom": 220.00 },
+                { "name": "E Equipment and Furnishings", "square_footage": 49050, "current_rom": 50.00 },
+                { "name": "F Special Construction", "square_footage": 10000, "current_rom": 80 },
+                { "name": "G Building Sitework", "square_footage": 49050, "current_rom": 40.72 }
             ]
         }
     },
     "benchmarks": [
         {
             "id": "A",
-            "name": "OSU Huang Collaborative Innovation Complex",
-            "overall_sf_cost": 840.00,
-            "square_footage": 152000,
+            "name": "WSU Schweitzer Engineering Hall",
+            "overall_sf_cost": 939.20,
+            "square_footage": 65000,
             "components": [
-                { "name": "A Substructure", "cost": 194.96 },
-                { "name": "B Shell", "cost": 178.08 },
-                { "name": "C Interiors", "cost": 158.48 },
-                { "name": "D Services", "cost": 297.14 },
-                { "name": "E Equipment and Furnishings", "cost": 54.32 },
-                { "name": "F Special Construction", "cost": 108.64 },
-                { "name": "G Building Sitework", "cost": 48.38 }
+                { "name": "A Substructure", "cost": 176.06 },
+                { "name": "B Shell", "cost": 200 },
+                { "name": "C Interiors", "cost": 108.12 },
+                { "name": "D Services", "cost": 227.34 },
+                { "name": "E Equipment and Furnishings", "cost": 49.06 },
+                { "name": "F Special Construction", "cost": 98.11 },
+                { "name": "G Building Sitework", "cost": 63 }
             ]
         },
         {
             "id": "B",
             "name": "WSU The Spark",
-            "overall_sf_cost": 872.10,
-            "square_footage": 83500,
+            "overall_sf_cost": 721.17,
+            "square_footage": 83000,
             "components": [
-                { "name": "A Substructure", "cost": 169.54 },
-                { "name": "B Shell", "cost": 187.64 },
-                { "name": "C Interiors", "cost": 167.70 },
-                { "name": "D Services", "cost": 240.31 },
-                { "name": "E Equipment and Furnishings", "cost": 57.46 },
-                { "name": "F Special Construction", "cost": 119.81 },
-                { "name": "G Building Sitework", "cost": 29.64 }
+                { "name": "A Substructure", "cost": 125.78 },
+                { "name": "B Shell", "cost": 205 },
+                { "name": "C Interiors", "cost": 85.42 },
+                { "name": "D Services", "cost": 205.28 },
+                { "name": "E Equipment and Furnishings", "cost": 42.63 },
+                { "name": "F Special Construction", "cost": 88.88 },
+                { "name": "G Building Sitework", "cost": 70 }
             ]
         },
         {
             "id": "C",
             "name": "WSU Everett",
-            "overall_sf_cost": 690.00,
+            "overall_sf_cost": 964.36,
             "square_footage": 96000,
             "components": [
-                { "name": "A Substructure", "cost": 153.25 },
-                { "name": "B Shell", "cost": 147.66 },
-                { "name": "C Interiors", "cost": 129.49 },
-                { "name": "D Services", "cost": 191.94 },
-                { "name": "E Equipment and Furnishings", "cost": 45.43 },
-                { "name": "F Special Construction", "cost": 90.05 },
-                { "name": "G Building Sitework", "cost": 32.20 }
+                { "name": "A Substructure", "cost": 187.06 },
+                { "name": "B Shell", "cost": 210.24 },
+                { "name": "C Interiors", "cost": 75.05 },
+                { "name": "D Services", "cost": 210.28 },
+                { "name": "E Equipment and Furnishings", "cost": 55.45 },
+                { "name": "F Special Construction", "cost": 109.91 },
+                { "name": "G Building Sitework", "cost": 39.31 }
             ]
         },
         {
             "id": "D",
             "name": "WSU Tri-Cities Collaboration Hall",
-            "overall_sf_cost": 643.80,
-            "square_footage": 58000,
+            "overall_sf_cost": 746.33,
+            "square_footage": 37300,
             "components": [
-                { "name": "A Substructure", "cost": 148.51 },
-                { "name": "B Shell", "cost": 136.86 },
-                { "name": "C Interiors", "cost": 121.32 },
-                { "name": "D Services", "cost": 171.72 },
-                { "name": "E Equipment and Furnishings", "cost": 90.63 },
-                { "name": "F Special Construction", "cost": 85.80 },
-                { "name": "G Building Sitework", "cost": 38.96 }
+                { "name": "A Substructure", "cost": 139.63 },
+                { "name": "B Shell", "cost": 190 },
+                { "name": "C Interiors", "cost": 87.06 },
+                { "name": "D Services", "cost": 205.45 },
+                { "name": "E Equipment and Furnishings", "cost": 85.21 },
+                { "name": "F Special Construction", "cost": 80.67 },
+                { "name": "G Building Sitework", "cost": 50 }
             ]
         }
     ]
